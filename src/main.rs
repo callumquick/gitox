bf2e0088fc510327109cc79ac0948d8deef9e4e2
Callum Ward <calward@cisco.com>
2020-09-21 08:54:00 +0100

    Refactor argparsing code to use clap macros instead of builder

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
-use clap::{App, SubCommand};
+#[macro_use]
+extern crate clap;
 
 mod data;
 
 fn main() {
-    let matches = App::new("gitox")
-        .version("1.0")
-        .author("Callum Ward <wards.callum@gmail.com")
-        .about("Git clone written in Rust for education")
-        .subcommand(SubCommand::with_name("init").about("Initialize the repository"))
-        .get_matches();
+    let matches = clap_app!(gitox =>
+        (version: "0.1.0")
+        (author: "Callum Ward <wards.callum@gmail.com")
+        (about: "Git clone written in Rust for education")
+        (@subcommand init =>
+            (about: "Initialize the repository")
+        )
+    )
+    .get_matches();
 
     if let Some(_matches) = matches.subcommand_matches("init") {
         data::init();
