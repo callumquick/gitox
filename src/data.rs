6af6ea33221ccb1e68fee9f6a67570a59e38579a
Callum Ward <calward@cisco.com>
2020-09-21 11:26:34 +0100

    Add hash-file and cat-file subcommands

--- src/data.rs
+++ src/data.rs
@@ -1,7 +1,22 @@
+use sha1::{Digest, Sha1};
 use std::fs;
 
 const GIT_DIR: &str = ".gitox";
+const OBJECT_DIR: &str = ".gitox/objects";
 
-pub fn init() {
-    fs::create_dir_all(GIT_DIR).unwrap();
+pub fn init() -> std::io::Result<()> {
+    fs::create_dir_all(GIT_DIR)?;
+    fs::create_dir_all(OBJECT_DIR)?;
+    Ok(())
+}
+
+pub fn hash_object(data: &[u8]) -> std::io::Result<String> {
+    let hash = Sha1::digest(data);
+    let oid = format!("{:x}", hash);
+    fs::write(format!("{}/{oid}", OBJECT_DIR, oid = oid), data)?;
+    Ok(oid)
+}
+
+pub fn get_object(oid: &str) -> std::io::Result<Vec<u8>> {
+    fs::read(format!("{}/{oid}", OBJECT_DIR, oid = oid))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 #[macro_use]
 extern crate clap;
 use clap::{Arg, SubCommand};
+use std::fs;
+use std::process::exit;
 
 mod data;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let matches = clap_app!(gitox =>
         (version: "0.1.0")
         (author: "Callum Ward <wards.callum@gmail.com")
@@ -17,16 +19,35 @@ fn main() {
     // they contain hyphens in the name
     .subcommand(
         SubCommand::with_name("hash-file")
-            .help("Hash a file into a stored object")
+            .about("Hash a file into a stored object")
             .arg(Arg::with_name("FILE").help("File to hash").required(true)),
+    )
+    .subcommand(
+        SubCommand::with_name("cat-file")
+            .about("Retrieve a stored object file")
+            .arg(
+                Arg::with_name("OBJECT")
+                    .help("Object to retrieve")
+                    .required(true),
+            ),
     )
     .get_matches();
 
     match matches.subcommand() {
         ("init", _) => data::init(),
         ("hash-file", Some(submatches)) => {
-            println!("Gonna hash {}", submatches.value_of("FILE").unwrap());
+            let oid = data::hash_object(&fs::read(submatches.value_of("FILE").unwrap())?)?;
+            println!("{}", oid);
+            Ok(())
         }
-        _ => println!("{}", matches.usage()),
+        ("cat-file", Some(submatches)) => {
+            let contents = data::get_object(submatches.value_of("OBJECT").unwrap())?;
+            print!("{}", String::from_utf8_lossy(&contents));
+            Ok(())
+        }
+        _ => {
+            eprintln!("{}", matches.usage());
+            exit(1);
+        }
     }
 }
